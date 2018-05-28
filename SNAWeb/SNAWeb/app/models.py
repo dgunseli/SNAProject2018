0fd44a86f3db0402f210e8897a7845042e97df34
Özgür Deniz Günseli <deniz.gunseli@mtdt.co>
2018-05-29 00:03:02 +0300

    *Bootstrap Added **Filter outline completed

--- SNAWeb/SNAWeb/app/models.py
+++ SNAWeb/SNAWeb/app/models.py
@@ -3,6 +3,14 @@ Definition of models.
 """
 
 from django.db import models
+from json import JSONEncoder
+
+def _default(self, obj):
+    return getattr(obj.__class__, "to_json", _default.default)(obj)
+
+_default.default = JSONEncoder.default  # Save unmodified default.
+JSONEncoder.default = _default # Replace it.
+
 
 # Create your models here.
 class SatisDokumBilgi(models.Model):
@@ -15,3 +23,42 @@ class SatisDokumBilgi(models.Model):
     sgketkinkod = models.TextField(blank=True, null=True)
     firma_id = models.IntegerField(blank=True, null=True)
     madde = models.TextField(blank=True, null=True)
+    def to_json(self):  
+        return '{{"madde": "{0}" ,"firma_id" : {1} }}'.format(self.madde, int(self.firma_id))
+    
+
+class EczaneBilgi(object):
+    eczane_adi = str
+    eczane_id = int
+    managed = False
+    def as_EczaneBilgi(dct):
+        bilgi = EczaneBilgi()
+        bilgi.eczane_adi = dct['eczane_adi']
+        bilgi.eczane_id = dct['eczane_id']
+        return bilgi
+    def __init__(self):
+        self.eczane_adi = ""
+        self.eczane_id =-1
+    def __init__(self,*eczane_adi,**eczane_id):
+        self.eczane_adi = eczane_adi
+        self.eczane_id =eczane_id
+    def to_json(self):  
+        return '{{"eczane_adi": "{0}" ,"eczane_id" : {1} }}'.format(self.eczane_adi, int(self.eczane_id))
+
+class FirmaBilgi(object):
+    firma_adi = str
+    firma_id = int
+    managed = False
+    def as_FirmaBilgi(dct):
+        bilgi = FirmaBilgi()
+        bilgi.firma_adi = dct['firma_adi']
+        bilgi.firma_id = dct['firma_id']
+        return bilgi
+    def __init__(self,*firma_adi,**firma_id):
+        self.firma_adi = firma_adi
+        self.firma_id = firma_id
+    def __init__(self):
+        self.firma_adi = ""
+        self.firma_id = -1
+    def to_json(self):  
+        return '{{"firma_adi": "{0}" ,"firma_id" : {1} }}'.format(self.firma_adi, int(self.firma_id))
--- SNAWeb/SNAWeb/app/views.py
+++ SNAWeb/SNAWeb/app/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 from django.http import HttpRequest
 from django.template import RequestContext
 from datetime import datetime
-from .models import SatisDokumBilgi
+from .models import SatisDokumBilgi, EczaneBilgi, FirmaBilgi
 from django.views.generic import TemplateView, ListView
 import csv
 import networkx as nx
@@ -14,8 +14,56 @@ from networkx.algorithms import bipartite
 import matplotlib.pyplot as plt
 from django.core import serializers
 import json
+from .helpers import *
+
+
 
 def home(request):
+    clearTest = request.GET.get('clearSession',None)
+    if (clearTest):
+        if(request.session.get('pharmacy_list',None) != None):
+            del request.session['pharmacy_list']
+        if(request.session.get('firm_list',None) != None):
+            del request.session['firm_list']
+        if(request.session.get('first_node',None) != None):
+            del request.session['first_node']
+        if(request.session.get('second_node',None) != None):
+            del request.session['second_node']
+        if(request.session.get('date_start',None) != None):
+            del request.session['date_start']
+        if(request.session.get('date_end',None) != None):
+            del request.session['date_end']
+        if(request.session.get('json_data',None) != None):
+            del request.session['json_data']
+    pharmacyList = []
+    firmList = []
+    firstNode =  request.session.get('first_node')
+    secondNode =  request.session.get('second_node')
+    dateStart =  request.session.get('date_start')
+    dateEnd =  request.session.get('date_end')
+    jsonGraphData = request.session.get('json_data')
+
+    if(request.session.get('pharmacy_list',None) == None):
+        pharmacyList = setPharmacySessionData(request)
+        request.session['pharmacy_list'] = json.dumps(pharmacyList)
+    else:
+        pharmacyList =  json.loads(request.session.get('pharmacy_list'))
+        pharmacyDecoded = []
+        for pharm in pharmacyList:
+            eBilgi = json.loads(pharm,object_hook = EczaneBilgi.as_EczaneBilgi)
+            pharmacyDecoded.append(eBilgi)
+        pharmacyList = pharmacyDecoded
+    if(request.session.get('firm_list',None) == None):
+        firmList = setFirmSessionData(request)
+        request.session['firm_list'] =  json.dumps(firmList)
+    else:
+        firmList =  json.loads(request.session.get('firm_list'))
+        firmListDecoded = []
+        for frm in firmList:
+            fBilgi = json.loads(frm,object_hook = FirmaBilgi.as_FirmaBilgi)
+            firmListDecoded.append(fBilgi)
+        firmList = firmListDecoded
+
     """Renders the home page."""
     graphData = SatisDokumBilgi.objects.filter(islem_tarihi__gte='2018-03-25')
     G = nx.Graph()
@@ -32,7 +80,9 @@ def home(request):
         {
             'title':'Ana Sayfa',
             'year':datetime.now().year,
-            'jsonData':jsonData
+            'jsonData':jsonData,
+            'eczaneListesi' : pharmacyList,
+            'firmaListesi' : firmList
         })
 
 def contact(request):
